f6c7e9414e92d553b16f1cd45c8b893150fb2a6c
Manan Kohli <manankohli77@gmail.com>
2021-12-18 14:24:11 +0530

    hello

--- src/com/manan/Temp.java
+++ src/com/manan/Temp.java
@@ -4,40 +4,30 @@ import java.util.Arrays;
 
 public class Temp {
     public static void main(String[] args) {
-        int[] nums = {3,2,2};
-        int i = 0;
-        int[] ans = {0, 0};
-        boolean present = false;
 
-        while (i < nums.length) {
-            int correct = nums[i] - 1;
-            if (nums[i] == 1) {
-                present = true;
-            }
-            if (nums[correct] == nums[i]) {
-                i++;
-            } else {
+        int[] nums = {7,8,9,11,12};
+        System.out.println(firstMissingPositive(nums));
+
+    }
+    public static int firstMissingPositive(int[] nums) {
+        int i = 0;
+        int ans = 0;
+        while(i< nums.length){
+            int correct  = nums[i] - 1;
+            if(nums[i]>0 && nums[correct] != nums[i] && nums[i]<= nums.length){
                 int temp = nums[correct];
                 nums[correct] = nums[i];
                 nums[i] = temp;
+            }else{
+                i++;
             }
         }
-        for (int j = 0; j < nums.length; j++) {
-            if(present) {
-                if (nums[j] != j + 1) {
-                    ans[0] = j;
-                    ans[1] = j + 1;
-                }
-            } else {
-                if (nums[j] != j + 2) {
-                    ans[0] = j+1;
-                    ans[1] = 1;
-                    break;
-                }
+        for(int j = 0;j<nums.length;j++){
+            if(nums[j]!=j+1){
+                ans = j+1;
             }
         }
-        System.out.println(Arrays.toString(ans));
-
+    return ans;
     }
 }
 
