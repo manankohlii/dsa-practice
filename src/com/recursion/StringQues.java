4b245d49eb67b4d8f919c9f028bffcdcb7ccc6d7
Manan Kohli <manankohli77@gmail.com>
2022-02-16 12:05:46 +0530

    added qs recursion

--- src/com/recursion/StringQues.java
+++ src/com/recursion/StringQues.java
@@ -3,6 +3,8 @@ package com.recursion;
 public class StringQues {
     public static void main(String[] args) {
         skip("","abcdabadc");
+        System.out.println(skipAlt("abcdabadc"));
+        System.out.println(skipApple("baapplemank"));
     }
     public static void skip(String p,String up){
         if(up.isEmpty()){
@@ -15,6 +17,28 @@ public class StringQues {
             skip(p,up.substring(1));       //sends string starting from next index
         }else{
             skip(p + ch,up.substring(1));
+        }
+    }
+    public static String skipAlt(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+        char ch = up.charAt(0);   //storing char at starting index in each iteration's string
+
+        if(ch=='a'){                   //removes occurrences of 'a'
+            return skipAlt(up.substring(1));       //sends string starting from next index
+        }else{
+            return ch + skipAlt(up.substring(1));
+        }
+    }
+    public static String skipApple(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+        if(up.startsWith("apple")){
+            return skipApple(up.substring(5));
+        }else{
+            return up.charAt(0) + skipApple(up.substring(1));
         }
     }
 }
