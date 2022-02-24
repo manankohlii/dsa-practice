360e8abab4251ae7a8c306ae8f1f1a80845a2f09
Manan Kohli <manankohli77@gmail.com>
2022-02-24 11:09:56 +0530

    added qs recursion

--- src/com/recursion/Subsets.java
+++ src/com/recursion/Subsets.java
@@ -1,12 +1,13 @@
 package com.recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Subsets {
     public static void main(String[] args) {
         //subseq("","abc");
-        int[] arr = {1, 2, 3};
+        int[] arr = {1, 2, 3}; // use [1,2,2] for duplicate function
         List<List<Integer>> ans = iterative(arr);
         for (List<Integer> list : ans) {
             System.out.println(list);
@@ -33,6 +34,28 @@ public class Subsets {
                 List<Integer> inner = new ArrayList<>(outer.get(i));  //to get copies of lists already present // i is for index
                 inner.add(num);
                 outer.add(inner);
+            }
+        }
+        return outer;
+    }
+    static List<List<Integer>> subsetDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            // if current and previous element is same, s = e + 1
+            if (i > 0 && arr[i] == arr[i-1]) {
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
             }
         }
         return outer;
